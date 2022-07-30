315609e058c98113113ffac254a26bfc739c5906
Vitor Gabriel de Oliveira <106203763+vitorgabrielgit@users.noreply.github.com>
2022-07-30 09:06:20 -0300

    Add files via upload

---
+++ js/scripts.js
@@ -0,0 +1,23 @@
+// button new Managers
+let tokenBTNnewObject = 0;
+let BTNnewObject = document.querySelector('#BTNnewObject').addEventListener('click', () => 
+{
+    tokenBTNnewObject++
+    if(tokenBTNnewObject <= 6)
+    {
+    let listItemsCreate = document.querySelector('#listItemsCreate');
+    let listItems = document.createElement("textarea");
+    listItems.spellcheck = false;
+    listItemsCreate.appendChild(listItems);
+    } else
+    {
+        alert('Maximo de lembretes !!')
+    }
+    let textarea = document.querySelector("textarea");
+    // listItems.appendChild(document.createTextNode(''));
+
+});
+
+let editDivs = document.querySelector("div").addEventListener('click',() => {
+    alert('hello')
+})
